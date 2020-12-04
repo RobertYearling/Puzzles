1efbeabd388bcc9d9c1474a7e3edaa095dab58da
Robert Yearling <laserguy@mac.com>
2020-12-04 17:10:28 -0600

    Console.WriteLines

--- Program.cs
+++ Program.cs
@@ -82,18 +82,27 @@ namespace Puzzles
 
         // ***** Name
 
-        public static bool isUnique(int[] someArray, int vall)
+        public static bool isUnique(int[] someArray, int val)
         {
+            foreach(int s in someArray)
+            {
+                Console.WriteLine(s);
+            }
+            Console.WriteLine("*****");
+            Console.WriteLine(val);
             bool True = true;
             int i = 0;
             while(True && i < someArray.Length)
             {
-                if(someArray[i] == vall)
+                if(someArray[i] == val)
                 {
-                    return True = false;
+                    True = false;
+                    Console.WriteLine("false");
+                    return True;
                 }
                 i++;
             }
+            Console.WriteLine("True");
             return True;
         }
         public static List<string> Names()
@@ -106,20 +115,38 @@ namespace Puzzles
                 "Geneva",
                 "Sydney"
             };
+            // foreach(string n in names)
+            // {
+            //     Console.WriteLine(n);
+            // }
             Random rand = new Random();
-            int[] newArr = new int[5]
-            {
-                -1,-1,-1,-1,-1
-            };
+            List<string> newList = new List<string>();
             int i = 0;
             int x = 0;
             while(x <= names.Count)
             {
-                int individuals = rand.Next(0,5);
-                if(isUnique(newArr, individuals))
+                int[] numsArr = new int[names.Count];
+                int individuals = rand.Next(1,6);
+                if(isUnique(numsArr, individuals) == true)
                 {
+
+                    Console.WriteLine("YOLO");
                     Console.WriteLine(individuals);
-                    newArr[i] = individuals;
+                    numsArr[i] = individuals;
+                    newList.Add(names[individuals]);
+                    i++;
+                    x++;
+                    Console.WriteLine(i);
+                    Console.WriteLine("OLOY");
+
+
+                }
+            }
+            foreach(string n in newList)
+            {
+                Console.WriteLine(n);
+            }
+            return newList;
                     if(names[i].Length < 5)
                     {
                         names.Remove(names[i]);
@@ -128,11 +155,6 @@ namespace Puzzles
                     x++;
                 }
             }
-            foreach(string n in names)
-            {
-                Console.WriteLine(n);
-            }
-            return names;
         }
     }
 }
